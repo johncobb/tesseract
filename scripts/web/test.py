2bdb8d9c31432526d8b98ac913967938721cb0c4
Tyler <tyler.m.meserve@gmail.com>
2019-08-21 14:59:01 -0500

    Cleaned up the API documentation

--- scripts/web/test.py
+++ scripts/web/test.py
@@ -1,11 +1,27 @@
 import requests, os
+import sys
 
 path = "/Users/tylermeserve/Documents/Tesseract/tesseract/out/tsv/0"
 files1 = [('file', open(os.path.join(path, "result-004.tsv"), 'rb')), ('file', open(os.path.join(path, 'result-006.tsv'), 'rb'))]
 files1 = [('file', open(os.path.join(path, file), 'rb')) for file in sorted(os.listdir(path))]
 params = {'id': 0, 'configid': 1}
+IP = ''
 
-r = requests.post('https://mmljg0chkd.execute-api.us-east-1.amazonaws.com/dev/api/upload', files=files1, params=params)
+if not os.path.isfile(os.path.join(os.getcwd(), 'server_secrets.txt')):
+    print('Go to server_secrets.txt and replace "(your api domain) with your api domain.')
+    with open(os.path.join(os.getcwd(), 'server_secrets.txt'), 'w+') as f:
+        f.write('IP=(your api domain)')
+        pass
+
+with open(os.path.join(os.getcwd(), 'server_secrets.txt'), 'r') as f:
+    for line in f.readlines():
+        if line.startswith('IP='):
+            IP = line.replace('IP=', '')
+            
+if not IP:
+    print('Go to server_secrets.txt and replace "(your api domain) with your api domain."')
+    sys.exit()
+r = requests.post('{0}/api/upload'.format(IP), files=files1, params=params)
 
 
 for file in files1:
